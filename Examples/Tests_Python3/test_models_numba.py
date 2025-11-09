c0052b5168fa7cd384c9f43f9943c33beca16636
gorkazl <gorka@Zamora-Lopez.xyz>
2025-11-10 00:28:16 +0100

    Update test_models_numba.py

--- Examples/Tests_Python3/test_models_numba.py
+++ Examples/Tests_Python3/test_models_numba.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2013 - 2022, Gorka Zamora-López <galib@Zamora-Lopez.xyz>
+# Copyright (c) 2013, Gorka Zamora-López <gorka@Zamora-Lopez.xyz>
 #
 # Released under the Apache License, Version 2.0 (the "License");
 # you may not use this software except in compliance with the License.
@@ -7,16 +7,12 @@
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 
-"""In this file I will test all functions in the Python3 version of GAlib
-(the module gamodels_numba.py) and make sure they work.
+"""In this file I will test all functions in module gamodels_numba.py.
 """
 
 # Standard library imports
-import os, os.path
 from timeit import default_timer as timer
 # Third party imports
-import matplotlib
-matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from numpy import*
 # Personal libraries
@@ -27,10 +23,8 @@ from galib.models_numba import RandomGraph_Numba
 
 ##################################################################
 # 0) READ THE DATA
-currdir = os.getcwd()
-currdir = os.path.split(currdir)[0]
-dataroot = os.path.join(currdir, 'Data/')
-net, labs = LoadFromPajek(dataroot + 'Cat53_cortex.net', True)
+dataroot = '../Data/'
+net, labs = LoadFromPajek(dataroot + 'Cat53_cortex.net', getlabels=True)
 netsym = 0.5*(net+net.T)
 N = len(net)
 
@@ -54,37 +48,13 @@ N = 5000
 # 2) RANDOM GRAPHS
 dens = 0.1
 L = int(0.5*dens*N*(N-1))
-# ernet = ErdosRenyiGraph(N, dens)
-# randnet = RandomGraph(N,L)
-randnet = RandomGraph_Numba(N,L)
-# sfnet = ScaleFreeGraph(N,dens, 3)
+randnet = RandomGraph_Numba(N,L, directed=False)
 
 
 # 3) RANDOM DIGRAPHS
 dens = 0.1
 L = int(dens*N*(N-1))
-# ernet = ErdosRenyiGraph(N, dens, directed=True)
-# randnet = RandomGraph(N,L, directed=True)
 randnet = RandomGraph_Numba(N,L, directed=True)
-# banet = BarabasiAlbertGraph(N,5)
-# sfnet = ScaleFreeGraph(N,dens, 3, directed=True)
-
-#
-# # 4) REWIRING ALGORITHMS
-# # rewnet = RewireNetwork(net)
-# # rewnet2 = ModularityPreservingGraph(net, partition)
-# rewnet = RewireNetwork(netsym)
-# rewnet2 = ModularityPreservingGraph(netsym, partition)
-# Qorig = Modularity(netsym.astype(bool), partition)
-# Qrew = Modularity(rewnet2.astype(bool), partition)
-# print('Modularity', Qorig, Qrew)
-#
-#
-# # 5) MODULAR & HIERARCHICAL NETS
-# modnet, newpart = ModularHeterogeneousGraph([100,200,400], [0.2,0.1,0.05], 0.02, directed=True)
-# # testpart = HMpartition([100,200,400])
-# hmrnet = HMRandomGraph([2,3,100], [2,10,20], directed=True)
-# hmcnet = HMCentralizedGraph([2,3,100], [2,10,20], [1.8,2.5,3.0], directed=False)
 
 time2 = timer()
 print( time2 - time1, 'Seconds' )
