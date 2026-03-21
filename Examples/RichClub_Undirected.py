6ef21c633711c6f534a9a0ac941e76104c0524d0
gorkazl <gorka@Zamora-Lopez.xyz>
2026-03-22 00:00:46 +0100

    Update RichClub_undirected example

--- Examples/RichClub_Undirected.py
+++ Examples/RichClub_Undirected.py
@@ -17,66 +17,116 @@ UNDIRECTED graphs considered only.
 # Third party imports
 import matplotlib.pyplot as plt
 import numpy as np
-# Personal libraries
-from galib import k_Density
-from galib.models import RewireNetwork
-from galib.tools import SymmetriseMatrix, LoadFromPajek
+# Local libraries
+import galib
 
 
 ##################################################################
 # 0) READ THE DATA
 dataroot = 'Data/'
-# net = loadtxt(dataroot + 'Zachary.txt', dtype=uint8)
-# net = LoadFromPajek(dataroot + 'Dolphins.net', getlabels=False)
-# net, labels = LoadFromPajek(dataroot + 'LesMiserables.net', getlabels=True)
-net = np.loadtxt(dataroot + 'Cat53_cortex.txt', dtype=np.uint8)
-net = SymmetriseMatrix(net)
+# net = np.loadtxt(dataroot + 'Zachary.txt', dtype=np.uint8)
+# net, labels = galib.tools.LoadFromPajek(dataroot + 'LesMiserables.net', getlabels=True)
+net = np.loadtxt(dataroot + 'Cat53_cortex_sym.txt').astype(np.uint8)
+
+# Get some basic properties of the network
 N = len(net)
+L = int( 0.5 * net.astype(bool).sum() )
+deg = galib.Degree(net)
 
 
-# 1) COMPUTE THE RICH-CLUB OF THE NETWORKS
-# 1.1) Rich-club of the empirical network.
-# Notice that 'net' is weighted but function RichClub ignores the weights.
-rcdens = k_Density(net, rctype='undirected')
+# 1) COMPUTE THE k-DENSITY OF THE NETWORKS
+# 1.1) k-density of the empirical graph
+# Notice that 'net' can be weighted but function RichClub ignores the weights.
+kdens = galib.k_Density(net, rctype='undirected')
 
-# 1.2) Rich-club in an ensemble of rewired networks for comparison
+# 1.2) Surrogates - Compute k-density in RANDOM graphs
 nrealiz = 100
-prewire = 10
-kmax = len(rcdens)
-rewrcdens = np.zeros((nrealiz,kmax), np.float64)
-for re in range(nrealiz):
-    # Generate a randomly rewired graph (conserving the degrees)
-    rewnet = RewireNetwork(net, prewire, directed=False)
 
-    # Compute the rich club of the rewired graph
-    rewrcdens[re] = k_Density(rewnet, rctype='undirected')
+kmax = len(kdens)
+klist = np.arange(kmax)
+
+deg_rand = np.zeros((nrealiz,N), np.uint64)
+kdens_rand = np.zeros((nrealiz,kmax), np.float64)
+for re in range(nrealiz):
+    # Generate the rewired surrogate graph (conserving the degrees)
+    randnet = galib.RandomGraph(N,L, directed=False)
+    deg_rand[re] = galib.Degree(randnet)
+
+    # Compute its k-density
+    spam = galib.k_Density(randnet, rctype='undirected')
+    kdens_rand[re,:len(spam)] = spam
 
 # Find the average rich-club and deviation for each k value
-maxrewphi = rewrcdens.max(axis=0)
-minrewphi = rewrcdens.min(axis=0)
-meanrewphi = rewrcdens.mean(axis=0)
-devrewphi = rewrcdens.std(axis=0)
+maxphi_rand = kdens_rand.max(axis=0)
+minphi_rand = kdens_rand.min(axis=0)
+meanphi_rand = kdens_rand.mean(axis=0)
+
+
+# 1.3) Surrogates - Compute k-density in REWIRED graphs
+nrealiz = 100
+prewire = 10
+deg_rew = np.zeros((nrealiz,N), np.uint64)
+kdens_rew = np.zeros((nrealiz,kmax), np.float64)
+for re in range(nrealiz):
+    # Generate the rewired surrogate graph (conserving the degrees)
+    rewnet = galib.RewireNetwork(net, prewire, directed=False)
+    deg_rew[re] = galib.Degree(rewnet)
+
+    # Compute its k-density
+    kdens_rew[re] = galib.k_Density(rewnet, rctype='undirected')
+
+# Find the average rich-club and deviation for each k value
+maxphi_rew = kdens_rew.max(axis=0)
+minphi_rew = kdens_rew.min(axis=0)
+meanphi_rew = kdens_rew.mean(axis=0)
+
 
 
 # 2) PLOT THE RESULTS. REQUIRES MATPLOTLIB!!
-plt.figure()
-colorlist = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b']
+plt.figure(figsize=(6.4,6.4))
 
-# 2.1) Plot the rich-club of the network
-klist = np.arange(kmax)
-plt.plot(klist, rcdens, color=colorlist[1], label='Original network')
+# 2.1) Plot the degree distributions
+plt.subplot(2,1,1)
+plt.hist(deg,              bins=N, range=[0,N], width=0.8, color='tab:red', alpha=0.8, align='mid', density=True , label='Empirical', zorder=0)
+plt.hist(deg_rand.ravel(), bins=N, range=[0,N], width=0.6, color='gray', alpha=0.5, align='mid', density=True, label='Random Graphs', zorder=2 )
+# plt.hist(deg_rew.ravel(),  bins=N, range=[0,N], width=0.6, color='tab:blue', alpha=0.8, align='mid', density=True, label='Rewired Grahs', zorder=1 )
+plt.xlim(-1,kmax+1)
+plt.ylabel('Probability')
+plt.legend(loc='upper right')
 
-# 2.2) Plot the mean rich-club of rewired networks with errorbars
-# plt.errorbar(klist,meanrewphi,devrewphi, color=colorlist[1], label='Rewired networks')
-plt.plot(klist, meanrewphi, color=colorlist[0], label='Rewired networks')
-plt.plot(klist, maxrewphi, '--', color=colorlist[0], zorder=0)
-plt.plot(klist, minrewphi, '--', color=colorlist[0], zorder=0)
 
-# 2.3) Eye candy
-plt.xlabel('Node degree', fontsize=14)
-plt.ylabel('$\phi$-density', fontsize=14)
-plt.legend(loc='upper left', fontsize=10)
-plt.grid()
+# 2.2) Plot the k-densities
+plt.subplot(2,1,2)
+plt.plot(klist, kdens, '.-', color='tab:red', label='Empirical')
+plt.plot(klist, meanphi_rand, color='gray', label='Random graphs')
+plt.fill_between(klist, minphi_rand, maxphi_rand, color='gray', alpha=0.3, linewidth=1)
+plt.plot(klist, meanphi_rew, color='tab:blue', label='Rewired graphs')
+plt.fill_between(klist, minphi_rew, maxphi_rew, color='tab:blue', alpha=0.3)
+plt.xlim(-1,kmax+1)
+plt.ylim(-0.02,1.05)
+plt.xlabel('Node Degree')
+plt.ylabel('k-density')
+plt.grid(axis='y', ls='-.', lw=0.5)
+plt.legend(loc='upper left')
+
+plt.tight_layout()
+
+
+
+# 3) IDENTIFY THE SET OF RICH-CLUB HUBS, WITH k-DENSITY LARGER THAN A THRESHOLD
+threshold = 0.8
+# Identify the degree at which k-density overcomes the given threshold
+
+if kdens.max() < threshold:
+    print( f"No rich-club found for k-density > {threshold:1.3f}" )
+else:
+    k_crit = np.where(kdens >= threshold)[0][0]
+
+    # Identify indices of the nodes forming the rich-club
+    richclub = np.where(deg >= k_crit)[0]
+    print( f'List of Rich-Club nodes with k-density >= {threshold:1.3f}' )
+    print( richclub )
 
 
 plt.show()
+##
